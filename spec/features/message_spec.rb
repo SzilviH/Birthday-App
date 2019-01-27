e25cc5cde3f649d12a8ee6b3153ec6162cf382ea
Szilvi Horvath <szil.horvath@gmail.com>
2019-01-27 17:57:41 +0000

    add message for users birthday

--- spec/features/message_spec.rb
+++ spec/features/message_spec.rb
@@ -1,12 +1,13 @@
 #As a user,
 #Once I submitted my details
 #I want to see how long it is until my birthday
+require 'date'
 
 feature 'Display message' do
   scenario 'see how many days until birthday' do
     visit('/')
     fill_in :name, with: 'Haruki'
-    fill_in :day, with: 26
+    fill_in :day, with: Date.today + 2
     select 'January', from: :month
     click_button 'Go!'
     expect(page).to have_content 'Your birthday will be in 2 days, Haruki.'
